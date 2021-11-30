84497ec3567a9042c9699ce410f9d2bd688b2ac4
Felipe Gama Danta <epilefgama2@gmail.com>
2021-11-30 20:00:14 -0300

    Exercicios 1 ao 4 e bonus 2

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-3-javascript-logica-de-programacao-e-algoritmos/scripts.js
@@ -0,0 +1,69 @@
+let n = 7;
+
+// Exercicio 1
+// let sequencia = "";
+// for (let index = 0 ; index < n ; index += 1) {
+//     sequencia += "*";
+// }
+// for (let cont = 0 ; cont < n ; cont += 1) {
+//     console.log(sequencia);
+// }
+
+// Exercicio 2
+// let sequencia = "";
+// for (let index = 0 ; index < n ; index += 1) {
+//     sequencia += "*";
+//     console.log(sequencia);
+// }
+
+// Exercicio 3
+// for (let index = 1 ; index <= n ; index += 1) {
+//     let sequencia = "";
+//     for (let cont = 0 ; cont <= (n-index) ; cont += 1) {
+//         sequencia += " "    
+//     } 
+//     for (let marc = 0 ; marc < index ; marc += 1) {
+//         sequencia += "*"
+//     }
+//     console.log(sequencia);
+// }
+
+// Exercicio 4
+// for (let index = 1 ; index <= n ; index += 2) {
+//     let sequencia = "";
+//     for (let cont = 0 ; cont < ((n-index)/2) ; cont += 1 ){
+//         sequencia += " ";
+//     }
+//     for (let marc = 0 ; marc < index ; marc += 1) {
+//         sequencia += "*"
+//     }
+//     console.log(sequencia);
+// }
+
+// Exercicio 4
+for (let index = 1 ; index <= n ; index += 2) {
+    let sequencia = "";
+    for (let cont = 0 ; cont < ((n-index)/2) ; cont += 1 ){
+        sequencia += " ";
+    }
+    for (let marc = 0 ; marc < index ; marc += 1) {
+        sequencia += "*"
+    }
+    console.log(sequencia);
+}
+
+
+
+// Exercicio Bonus 2
+// let divisores = 0
+// let number = 31;
+// for (let divisor = 1 ; divisor <= number ; divisor += 1) {
+//     if (number % divisor === 0) {
+//         divisores += 1;
+//     }
+// }
+// if (divisores === 2) {
+//     console.log("É primo");
+// } else {
+//     console.log("Não é primo");
+// }
